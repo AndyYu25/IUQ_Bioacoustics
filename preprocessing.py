ae11b59e55acd167f7f9cbf1aab4dc5dc7cb39c2
Andy Yu <ayu303@gatech.edu>
2025-03-10 18:11:13 -0400

    Update preprocessing.py
    Add overall preprocessing function

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,10 @@
-import numpy as np
 import torchaudio
 from torchaudio.functional import resample
 from torchaudio.transforms import MelSpectrogram
 from torch.nn.functional import pad
-import torch
+from torch import save, Tensor
 import os
+import csv
 """
 Preprocessing approach:
 Liccardi & Carbone 2024: https://arxiv.org/html/2402.17775v2 - Mel Spectrograms only
@@ -17,7 +17,7 @@ Liccardi & Carbone 2024: https://arxiv.org/html/2402.17775v2 - Mel Spectrograms 
     b. hop-length set to 200
 Output: 64x41 images
 """
-def alignAudio(waveform: torch.Tensor, numTimestamps: int = 8000)->torch.Tensor:
+def alignAudio(waveform: Tensor, numTimestamps: int = 8000)->Tensor:
     """
     Aligns and centers the given audio to 8000 timestamps
     zero-pads shorter time series
@@ -31,19 +31,17 @@ def alignAudio(waveform: torch.Tensor, numTimestamps: int = 8000)->torch.Tensor:
         padLeft = deltaTimestamps // 2
         padRight = deltaTimestamps - padLeft
         #return padded tensor
-        print(padLeft, padRight)
         return pad(waveform, (padLeft, padRight))
-    else: #case of longer time series to zero-pad
+    else: #case of longer time series to crop
         #central idx of current waveform tensor
         timestampCenter = currTimestamps // 2
         #get values (relative to the centerpoint) to crop
         cropLeft = numTimestamps // 2
         cropRight = numTimestamps - cropLeft
-        print(timestampCenter - cropLeft, timestampCenter+cropRight)
         return waveform[:, timestampCenter - cropLeft:timestampCenter+cropRight]
 
 def preprocessFile(loadPath: str, melSpec: MelSpectrogram, resampleRate: int = 47600, 
-                   numTimestamps: int = 8000)->torch.Tensor:
+                   numTimestamps: int = 8000)->Tensor:
     """
     Performs preprocessing into a mel spectrogram on an individual file; returns the melSpectrogram\n
     loadPath: file path to load the audio from
@@ -54,16 +52,55 @@ def preprocessFile(loadPath: str, melSpec: MelSpectrogram, resampleRate: int = 4
     waveform, sampleRate = torchaudio.load(loadPath)
     #resampling to 47.6 kHz
     waveform = resample(waveform, sampleRate, resampleRate)
-    print(waveform.shape)
     #alignment & cropping
     waveform = alignAudio(waveform, numTimestamps)
     #conversion to mel spectrogram
-    print(waveform.shape)
     melSpecOut = melSpec.forward(waveform)
-    print(melSpecOut.shape)
     return melSpecOut
 
-#frequency/ n_mels = 64; hop_length = 200
-#melspecConversion = MelSpectrogram(n_mels = 64, hop_length = 200, normalized = True)
-#path = os.path.join(os.getcwd(), "RawSoundData\\Atlantic Spotted Dolphin\\6102500D.wav")
-#preprocessFile(path, melspecConversion)
+def preprocessAudio(audioDir: str, outputDir: str, csvpath : str,
+                    n_mels:int = 64, hop_length:int = 200, normalized:bool = True,
+                    resampleRate:int = 47600, numTimestamps:int = 8000)->None:
+    """
+    Preprocess all sound files from audioDir and output them into outputDir as pytorch Tensors. 
+    Also aggregate each file's corresponding label (represented by the subfolder) into a csv,
+    Assumes one layer of subfolders exists (as generated from dataCollection.downloadDataset)
+    n_mels, hop_length, and normalized correspond to the mel spectrogram parameters,
+    resampleRate and numTimestamps affects the resampled rate and the number of timestampes respectively.
+    """
+    assert(os.path.exists(audioDir)) #check that that input dir exists
+    if not os.path.exists(outputDir): # create output directory if none exists
+        os.mkdir(outputDir)
+    fileLabelPair = [["fileName", "audioDir"]]
+    #instantiate melspectrogram
+    melSpec= MelSpectrogram(n_mels = n_mels, hop_length = hop_length, normalized = normalized)
+
+    for subfolder in os.listdir(audioDir): #iterate over each subfolder
+        #path to the corresponding subfolder in the output director
+        outSubdir = os.path.join(outputDir, subfolder)
+        if not os.path.exists(outSubdir): # create output subdir if none exists
+            os.mkdir(outSubdir)
+        for file in os.listdir(os.path.join(audioDir, subfolder)):
+            #associate each file with its label/parent directory name
+            fileName = os.path.splitext(file)[0]
+            fileLabelPair.append([fileName, subfolder])
+            audioPath = os.path.join(audioDir, subfolder, file)
+            print(audioPath)
+            #initialize the output file path
+            outFileName = fileName + ".pt"
+            outFilePath = os.path.join(outSubdir, outFileName)
+            print(outFilePath)
+            outSpectrogram = preprocessFile(audioPath, melSpec, resampleRate, numTimestamps)
+            save(outSpectrogram, outFilePath)
+    #save label csv
+    with open(csvpath, "w") as file:
+        csvWriter = csv.writer(file)
+        csvWriter.writerows(fileLabelPair)
+
+
+if __name__ == "__main__":
+    #frequency/ n_mels = 64; hop_length = 200
+    #melspecConversion = MelSpectrogram(n_mels = 64, hop_length = 200, normalized = True)
+    #path = os.path.join(os.getcwd(), "RawSoundData\\Atlantic Spotted Dolphin\\6102500D.wav")
+    #preprocessFile(path, melspecConversion)
+    preprocessAudio("testDir", "testOutDir", "labels.csv")
